4d4bec8c251d8af317fa5c23ab3a089c648a19bc
Firas Mansouri <mansouri.firas@esprit.tn>
2022-03-01 22:13:27 +0100

    i created the main page with javafx the added files are main.fxml/homePage.fxml/matchs.fxml/news.fxml/store.fxml/teams.fxml ; MainController.java/HomeController.java/MatchsController.java/NewsC ontroller.java/TeamsController.java/StoreController.java; TestFxml.java ********** the main page contains to navbars: one topNavbar and onesideNovbar wich contain home,teams,matchs,news,store menus

--- ggesports/src/test/Ggesports.java
+++ ggesports/src/test/Ggesports.java
@@ -15,7 +15,7 @@ public class Ggesports {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here        
     }
     
 }
