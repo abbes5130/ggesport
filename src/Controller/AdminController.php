f0e7d6f69b862224924842bd0be8cc49057b613b
unknown <ghassene.badra@esprit.com>
2022-04-11 01:49:38 +0100

    Crud + Template

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -10,11 +10,13 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 /**
- * @Route("/admin", name="app_admin")
+ * @Route("/admin")
  */
 class AdminController extends AbstractController
 {
-
+    /**
+     * @Route("/", name="app_admin", methods={"GET"})
+     */
     public function index(): Response
     {
         return $this->render('/back_base.html.twig', [
@@ -22,5 +24,89 @@ class AdminController extends AbstractController
         ]);
     }
 
+
+    /**
+     * @Route("/AllMatch", name="app_admin_match_index", methods={"GET"})
+     */
+    public function indexAdmin(EntityManagerInterface $entityManager): Response
+    {
+        $matches = $entityManager
+            ->getRepository(Matches::class)
+            ->findAll();
+
+        return $this->render('/admin/AllMatchs.html.twig', [
+            'matches' => $matches,
+        ]);
+    }
+
+
+
+    /**
+     * @Route("/new", name="app_matches_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $match = new Matches();
+        $form = $this->createForm(MatchesType::class, $match);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($match);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_matches_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('matches/new.html.twig', [
+            'match' => $match,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/AllMatch/{idMatch}", name="app_matches_show", methods={"GET"})
+     */
+    public function show(Matches $match): Response
+    {
+        return $this->render('matches/show.html.twig', [
+            'match' => $match,
+        ]);
+    }
+
+
+
+
+    /**
+     * @Route("/{idMatch}/edit", name="app_matches_edit", methods={"GET", "POST"})
+     */
+    public function edit(Request $request, Matches $match, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(MatchesType::class, $match);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_matches_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('matches/edit.html.twig', [
+            'match' => $match,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{idMatch}", name="app_matches_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Matches $match, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$match->getIdMatch(), $request->request->get('_token'))) {
+            $entityManager->remove($match);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_matches_index', [], Response::HTTP_SEE_OTHER);
+    }
 
 }
--- src/Controller/MatchesController.php
+++ src/Controller/MatchesController.php
@@ -29,91 +29,16 @@ class MatchesController extends AbstractController
         ]);
     }
 
-    /**
-     * @Route("/match", name="app_matches_admin_index", methods={"GET"})
-     */
-    public function indexAdmin(EntityManagerInterface $entityManager): Response
-    {
-        $matches = $entityManager
-            ->getRepository(Matches::class)
-            ->findAll();
-
-        return $this->render('/admin/AllMatchs.html.twig', [
-            'matches' => $matches,
-        ]);
-    }
 
     /**
-     * @Route("/new", name="app_matches_new", methods={"GET", "POST"})
-     */
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $match = new Matches();
-        $form = $this->createForm(MatchesType::class, $match);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($match);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_matches_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('matches/new.html.twig', [
-            'match' => $match,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{idMatch}", name="app_matches_show", methods={"GET"})
-     */
-    public function show(Matches $match): Response
-    {
-        return $this->render('matches/show.html.twig', [
-            'match' => $match,
-        ]);
-    }
-
-    /**
-     * @Route("match/{idMatch}", name="app_matches_detail_show", methods={"GET"})
+     * @Route("/matchDetail/{idMatch}", name="app_matches_detail_show", methods={"GET"})
      */
     public function showDetail(Matches $match): Response
     {
-        return $this->render('/matches/match_detail.html.twig', [
+
+        return $this->render('matches/match_detail.html.twig', [
             'match' => $match,
         ]);
     }
-    /**
-     * @Route("/{idMatch}/edit", name="app_matches_edit", methods={"GET", "POST"})
-     */
-    public function edit(Request $request, Matches $match, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(MatchesType::class, $match);
-        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_matches_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->render('matches/edit.html.twig', [
-            'match' => $match,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{idMatch}", name="app_matches_delete", methods={"POST"})
-     */
-    public function delete(Request $request, Matches $match, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$match->getIdMatch(), $request->request->get('_token'))) {
-            $entityManager->remove($match);
-            $entityManager->flush();
-        }
-
-        return $this->redirectToRoute('app_matches_index', [], Response::HTTP_SEE_OTHER);
-    }
 }
